73047b8cca0d317217f84428b3e3e52d2417d142
cook1e <vincent.schweiger@xolley.de>
2021-04-30 11:07:29 +0200

    stuff

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
 
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
 
+#include <kernel/interrupts.h>
 #include <kernel/tty.h>
 
-static inline bool are_interrupts_enabled()
-{
-    unsigned long flags;
-    asm volatile ( "pushf\n\t"
-                   "pop %0"
-                   : "=g"(flags) );
-    return flags & (1 << 9);
+static inline bool are_interrupts_enabled() {
+  unsigned long flags;
+  asm volatile("pushf\n\t"
+               "pop %0"
+               : "=g"(flags));
+  return flags & (1 << 9);
 }
 
 void kernel_main(void) {
   terminal_initialize();
   printf("Hello, World!\n");
+  printf("OS Starting!\n");
+  idt_init();
   if (are_interrupts_enabled()) {
-	  printf("Interrupts are enabled\n");
+    printf("Interrupts are enabled\n");
   } else {
-	  printf("Interrupts are disabled\n");
+    printf("Interrupts are disabled\n");
   }
+  printf("OS Started!\n");
 }
-
-
